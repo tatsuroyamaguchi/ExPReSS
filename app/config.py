dfb4756fe20c1d26565d9ff670ab37d72bf8cdc1
tatsuroyamaguchi <76607586+tatsuroyamaguchi@users.noreply.github.com>
2025-06-05 14:49:27 +0900

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,16 +4,16 @@
 NORMAL_SAMPLES = ["口腔粘膜", "爪", "毛髪", "皮膚", "Tリンパ球", "その他", "不明"]
 
 # EP実施医療機関のデフォルト値
-DEFAULT_EP_INSTITUTION = "がん・感染症センター都立駒込病院"
+DEFAULT_EP_INSTITUTION = "がんゲノム病院"
 
 # EP実施責任診療科の選択肢
 EP_DEPARTMENTS = "遺伝子診療科"
 
 # EP実施責任者の選択肢
-EP_RESPONSIBLES = "山口 達郎"
+EP_RESPONSIBLES = "駒込 太郎"
 
 # 問い合わせ窓口のデフォルト値
-DEFAULT_EP_CONTACT = "〒113-8677  東京都文京区本駒込3-18-22"
+DEFAULT_EP_CONTACT = "〒163-8001 東京都新宿区西新宿2丁目8−1"
 
 # 電話番号のデフォルト値
-DEFAULT_EP_TEL = "03-3823-2101（代表）"
+DEFAULT_EP_TEL = "03-5321-1111（代表）"
